9871d04bcb5adbf1115ddf80b5fd1a6b1eeb0085
Rafael Garcia <dev.ragare@gmail.com>
2019-08-25 19:21:53 +0200

    before connecting store

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
+
+const store = configureStore()
+// add water bill
+store.dispatch(addExpense({
+    description: 'Water bill',
+    amount: 23000
+}))
+
+// add gas bill
+store.dispatch(addExpense({
+    description: 'Gas bill',
+    amount: 15000
+}))
+
+// set text filter
+store.dispatch(setTextFilter('bill'))
+
+// getVisibleExpenses
+const { expenses, filters } = store.getState()
+console.log(getVisibleExpenses(expenses, filters))
+
+
+console.log(store.getState())
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,16 +1,36 @@
 import { createStore } from 'redux'
 
-const store = createStore((state = { count: 0 }, action) => {
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+})
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+})
+
+const setCount = ({ count = 0 }) => ({
+    type: 'SET',
+    count
+})
+
+const resetCount = () => ({
+    type: 'RESET'
+})
+
+// Reducers
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             }
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             }
         case 'RESET':
             return {
@@ -23,8 +43,10 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state
     }
+}
 
-})
+
+const store = createStore(countReducer)
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState())
@@ -32,29 +54,14 @@ const unsubscribe = store.subscribe(() => {
 
 
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-})
+store.dispatch(incrementCount({ incrementBy: 5 }))
 
-store.dispatch({
-    type: 'INCREMENT'
-})
+store.dispatch(incrementCount())
 
-store.dispatch({
-    type: 'RESET'
-})
+store.dispatch(resetCount())
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-})
+store.dispatch(decrementCount({ decrementBy: 10 }))
 
-store.dispatch({
-    type: 'DECREMENT'
-})
+store.dispatch(decrementCount())
 
-store.dispatch({
-    type: 'SET',
-    count: 11
-})
+store.dispatch(setCount({ count: 11 }))
