72a790d9b171bd4a7d98bf15c43ab965ade2fab1
Rafael Garcia <dev.ragare@gmail.com>
2019-08-25 09:11:41 +0200

    before refactoring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,10 +1,53 @@
-const person = {
-    name: 'Andrew',
-    age: 26,
-    location: {
-        city: 'Philadelphia',
-        temp: 92
-    }
-}
+//
+// Object destructuring
+//
 
-console.log(`${person.name} is ${person.age}`)
+
+// const person = {
+//     name: 'Amdrew2',
+//     age: 26,
+//     location: {
+//         city: 'Philadelphia',
+//         temp: 88
+//     }
+// }
+
+// const { name: firstName = 'Anonymous', age } = person
+
+// console.log(`${firstName} is ${age}`)
+
+// const { temp: temperature, city } = person.location
+
+// if (temperature && city) {
+//     console.log(`It's ${temperature} in ${city}`)
+// }
+
+// const book = {
+//     title: 'Ego is the enemy.',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
+
+// const { name: publisherName = 'Self-Published' } = book.publisher
+
+// console.log(publisherName)
+
+//
+// Array destructuring
+//
+
+// const address = ['1299 S Juniper Strett', 'Philadelphia', 'Pennsylvania', '19147']
+
+// //const [street, city, state, zip] = address
+// const [, city, state] = address
+// //const [, city, state='New York'] = address
+
+// console.log(`You are in ${city} ${state}`)
+
+const item = ['Cofee (ice)', '$2.00', '$2.52', '$2.75']
+
+const [product, , medium] = item
+
+console.log(`A medium ${product} costs ${medium}`)
