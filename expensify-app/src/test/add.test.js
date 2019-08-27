627293b17c05eea1d1cade9400070767bd07e7c3
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-08-27 12:41:22 +0200

    113 Testing expense actions

--- expensify-app/src/test/add.test.js
+++ expensify-app/src/test/add.test.js
@@ -1,6 +1,7 @@
 const add = (a, b) => a + b 
 const generateGreeting = (name='Anonymous') => `Hello ${name}!`
 
+
 test('should add two numbers', () => {
     const result = add(3, 4)
     expect(result).toBe(7)
